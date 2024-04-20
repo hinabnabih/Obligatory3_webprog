2ffaaa9e9c1f0eeeab34cb6862250d76add0d86b
Hiban <hinab4897@oslomet.no>
2024-04-21 00:20:41 +0200

    sql for BOOKE

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Booke (film, kvantitet, fornavn, etternavn, telefonnummer, email) VALUES
+    ('Film 1', '2', 'Hiba', 'Nabih', '45913821', 'hibanabih@example.com');
+INSERT INTO Booke (film, kvantitet, fornavn, etternavn, telefonnummer, email) VALUES
+    ('Film 2', '1', 'Aya', 'Zahrani', '97902121', 'Ayazahani@example.com');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS  Booke (
-    id INTEGER AUTO_INCREMENT NOT NULL,
-    film VARCHAR(255),
-    kvantitet VARCHAR(225) NOT NULL,
-    fornavn VARCHAR(255),
-    etternavn VARCHAR(255),
-    telefonnummer VARCHAR(255),
-    email VARCHAR(255),
-    primary key (id)
+ id INTEGER AUTO_INCREMENT NOT NULL,
+ film VARCHAR(255),
+ kvantitet VARCHAR(225) NOT NULL,
+ fornavn VARCHAR(255),
+ etternavn VARCHAR(255),
+ telefonnummer VARCHAR(255),
+ email VARCHAR(255),
+ primary key (id)
 );
